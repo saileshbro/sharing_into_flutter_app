d0ef2c0ffa99c61547ccbfb56994ef8f2e3c73b2
Sailesh Dahal <saileshbro@gmail.com>
2023-02-15 19:16:48 +0545

    docs: add README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# 💪 Sharing into Flutter App
+
+![Cover image](https://cdn.hashnode.com/res/hashnode/image/upload/v1676265778828/26014b9d-cf49-48ba-b601-03022f207aa4.png)
+
+This is a flutter app demonstrating how to share media from other apps into your flutter app.
+
+<br>
+
+## Blog Post 📕
+
+> [**`🤳 Effortless Sharing: From external apps to your Flutter app in no time`**](https://saileshdahal.com.np/sharing-media-from-external-to-flutter-app)
+
+<br>
+
+## 📷 Demonstration
+
+![Demo image](https://cdn.hashnode.com/res/hashnode/image/upload/v1676290453387/c35ad321-b438-4991-b90e-02fc03cff5af.gif)
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,16 +1,85 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:share_handler/share_handler.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  StreamSubscription<SharedMedia>? _streamSubscription;
+  SharedMedia? media;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription?.cancel();
+    super.dispose();
+  }
+
+  Future<void> initPlatformState() async {
+    final handler = ShareHandlerPlatform.instance;
+    media = await handler.getInitialSharedMedia();
+    _streamSubscription = handler.sharedMediaStream.listen((SharedMedia media) {
+      if (!mounted) return;
+      setState(() => this.media = media);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
+        title: const Text('Share Handler'),
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: <Widget>[
+          Text(
+            'Shared to conversation identifier: ${media?.conversationIdentifier}',
+          ),
+          const SizedBox(height: 10),
+          Text('Shared text: ${media?.content}'),
+          const SizedBox(height: 10),
+          Text('Shared files: ${media?.attachments?.length}'),
+          ...(media?.attachments ?? []).map((attachment) {
+            final path = attachment?.path;
+            if (path != null &&
+                attachment?.type == SharedAttachmentType.image) {
+              return Column(
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      ShareHandlerPlatform.instance.recordSentMessage(
+                        conversationIdentifier:
+                            'custom-conversation-identifier',
+                        conversationName: 'John Doe',
+                        conversationImageFilePath: path,
+                        serviceName: 'custom-service-name',
+                      );
+                    },
+                    child: const Text('Record message'),
+                  ),
+                  const SizedBox(height: 10),
+                  Image.file(File(path)),
+                ],
+              );
+            }
+            return Text(
+              '${attachment?.type} Attachment: ${attachment?.path}',
+            );
+          }),
+        ],
       ),
     );
   }
